d7c2bd34d4a19b27cafd558bb69c0cd535128b93
GinShiro7th <reretiv@gmail.com>
2023-09-06 22:13:36 +0300

    Update webApp.js

--- webApp.js
+++ webApp.js
@@ -3,20 +3,6 @@ tg.ready()
 
 const fileInput = document.getElementById('fileToUpload');
 
-fileInput.addEventListener('change', (event) => {
-  const selectedFile = event.target.files[0];
-  if (selectedFile) {
-    const fileName = selectedFile.name;
-    console.log(`Выбранный файл: ${fileName}`);
-  } else {
-    console.log('Файл не был выбран');
-  }
-});
-
-
 function sendDataToTg(){
-  
-  const file = document.getElementById("fileToUpload");
-  alert(file.target.name);
   tg.sendData('123');
 }
