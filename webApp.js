9a51e623603d0533c31a6238dcc6c5609758737e
GinShiro7th <reretiv@gmail.com>
2023-09-06 21:49:14 +0300

    Update webApp.js

--- webApp.js
+++ webApp.js
@@ -1,9 +1,21 @@
 const tg = window.Telegram.WebApp;
 tg.ready()
 
+const fileInput = document.getElementById('fileToUpload');
+
+fileInput.addEventListener('change', (event) => {
+  const selectedFile = event.target.files[0];
+  if (selectedFile) {
+    const fileName = selectedFile.name;
+    alert(`Выбранный файл: ${fileName}`);
+  } else {
+    alert('Файл не был выбран');
+  }
+});
+
 function sendDataToTg(){
   
   const file = document.getElementById("fileToUpload");
-  alert(file);
+  file.
   tg.sendData('123');
 }
