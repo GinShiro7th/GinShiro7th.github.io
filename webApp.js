9774e694f813e9f885683627e280120893861148
GinShiro7th <reretiv@gmail.com>
2023-09-07 22:03:58 +0300

    Update webApp.js

--- webApp.js
+++ webApp.js
@@ -12,23 +12,22 @@ function sendDataToTg() {
 
   const formData = new FormData();
   formData.append("file", file);
-  tg.sendData(JSON.stringify(file.name));
-
-  // fetch("/localhost:3000", {
-  //   method: "POST",
-  //   body: formData,
-  // })
-  //   .then((response) => {
-  //     if (response.ok) {
-  //       tg.sendData(response.json());
-  //     } else {
-  //       throw new Error("Ошибка при отправке файла.");
-  //     }
-  //   })
-  //   .then((data) => {
-  //     console.log("Файл успешно загружен:", data);
-  //   })
-  //   .catch((error) => {
-  //     tg.sendData("Ошибка: " + error);
-  //   });
+  
+  fetch("localhost:3000", {
+    method: "POST",
+    body: formData,
+  })
+    .then((response) => {
+      if (response.ok) {
+        tg.sendData(response.json());
+      } else {
+        throw new Error("Ошибка при отправке файла.");
+      }
+    })
+    .then((data) => {
+      console.log("Файл успешно загружен:", data);
+    })
+    .catch((error) => {
+      tg.sendData("Ошибка: " + error);
+    });
 }
