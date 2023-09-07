1e76118df9a798ec67a0151ed806ec57165ccdfd
GinShiro7th <reretiv@gmail.com>
2023-09-07 20:47:40 +0300

    Update webApp.js

--- webApp.js
+++ webApp.js
@@ -4,7 +4,7 @@ tg.ready();
 function sendDataToTg() {
   const fileInput = document.getElementById("fileToUpload");
   const file = fileInput.files[0];
-
+  
   if (!file) {
     alert("Выберите ZIP-архив для загрузки.");
     return;
@@ -12,23 +12,23 @@ function sendDataToTg() {
 
   const formData = new FormData();
   formData.append("file", file);
-  tg.sendData(JSON.stringify(formData));
+  tg.sendData(JSON.stringify(file.name));
 
-  fetch("/localhost:3000", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (response.ok) {
-        tg.sendData(response.json());
-      } else {
-        throw new Error("Ошибка при отправке файла.");
-      }
-    })
-    .then((data) => {
-      console.log("Файл успешно загружен:", data);
-    })
-    .catch((error) => {
-      tg.sendData("Ошибка: " + error);
-    });
+  // fetch("/localhost:3000", {
+  //   method: "POST",
+  //   body: formData,
+  // })
+  //   .then((response) => {
+  //     if (response.ok) {
+  //       tg.sendData(response.json());
+  //     } else {
+  //       throw new Error("Ошибка при отправке файла.");
+  //     }
+  //   })
+  //   .then((data) => {
+  //     console.log("Файл успешно загружен:", data);
+  //   })
+  //   .catch((error) => {
+  //     tg.sendData("Ошибка: " + error);
+  //   });
 }
